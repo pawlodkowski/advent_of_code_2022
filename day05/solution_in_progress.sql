da435d30ad313cd4aa63f3284f40c661da74ec1a
Paul Wlodkowski <pawlodkowski@gmail.com>
2022-12-05 21:26:44 +0100

    day5 still WIP - absolutely brutal data processing

--- day05/solution_in_progress.sql
+++ day05/solution_in_progress.sql
@@ -21,19 +21,19 @@ WITH _unnested_crate_rows AS (
 	FROM day05.crates
 ),
 _unnested_crates AS (
-	select
+	SELECT
 		*,
-		row_number() over(partition by row) as col
-	from _unnested_crate_rows
+		row_number() OVER(PARTITION BY row) AS col
+	FROM _unnested_crate_rows
 ),
-unnested_crates as (
-	select 
+unnested_crates AS (
+	SELECT
 		*
-	from _unnested_crates
-	where mod(col, 4) = 2 
+	FROM _unnested_crates
+	WHERE mod(col, 4) = 2 
 	-- positions of the actual letters -> 2, 6, 10, 14, etc.
 ),
-unnested_crates_parsed as (
+unnested_crates_parsed AS (
 	--	 row_num | crate | stack_num 
 	--	---------+-------+-----------
 	--	       1 |       |         0
@@ -45,31 +45,31 @@ unnested_crates_parsed as (
 	--	       3 | Z     |         0
 	--	       3 | M     |         1
 	--	       3 | P     |         2
-	select 
-		row as row_num,
+	SELECT 
+		row AS row_num,
 		crate,
-		floor(col/4) as stack_num
-	from unnested_crates
+		floor(col/4) AS stack_num
+	FROM unnested_crates
 ),
-stack_data as (
-	select
+stack_data AS (
+	SELECT
 		stack_num,
-		string_agg(crate, '' order by row_num desc) as stack_contents
-	from unnested_crates_parsed
-	where crate <> ' '
-	group by 1
+		string_agg(crate, '' ORDER BY row_num DESC) AS stack_contents
+	FROM unnested_crates_parsed
+	WHERE crate <> ' '
+	GROUP BY 1
 ),
-move_vals as (
-	select 
-		regexp_match(sentence, 'move (\d+) from (\d+) to (\d+)') as vals
-	from day05.instructions
+move_vals AS (
+	SELECT 
+		regexp_match(sentence, 'move (\d+) from (\d+) to (\d+)') AS vals
+	FROM day05.instructions
 ),
-moves as (
-	select 
-		vals[1]::int as amount,
-		vals[2]::int as origin,
-		vals[3]::int as destination
-	from move_vals
+moves AS (
+	SELECT 
+		vals[1]::int AS amount,
+		vals[2]::int AS origin,
+		vals[3]::int AS destination
+	FROM move_vals
 )
-select * from stack_data;
+SELECT * FROM stack_data;
 
