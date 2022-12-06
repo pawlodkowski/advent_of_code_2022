d03d8621845fc017dc3c8c3342d02b51b87d1739
Paul Wlodkowski <pawlodkowski@gmail.com>
2022-12-06 10:43:32 +0100

    day05 pt 1 🤯

--- day05/solution_in_progress.sql
+++ day05/solution_in_progress.sql
@@ -7,14 +7,15 @@ CREATE TABLE day05.crates (
 );
 
 CREATE TABLE day05.instructions (
+	move     SERIAL,
   sentence TEXT
 );
 
 --TODO: dynamic numbers for -A and -B commands; currently hacky but it works (just has to be large enough)
 \COPY day05.crates (stacks) FROM PROGRAM 'grep -B100 "^[0-9]*$" day05/input.txt | tail -r | tail -n +3 | tail -r';
-\COPY day05.instructions    FROM PROGRAM 'grep -A200 "^[0-9]*$" day05/input.txt | tail -n +2';
+\COPY day05.instructions (sentence) FROM PROGRAM 'grep -A1000 "^[0-9]*$" day05/input.txt | tail -n +2';
 
-WITH _unnested_crate_rows AS (
+WITH RECURSIVE _unnested_crate_rows AS (
 	SELECT 
 	  row,
 	  unnest(REGEXP_SPLIT_TO_ARRAY(stacks, '')) AS crate
@@ -52,6 +53,11 @@ unnested_crates_parsed AS (
 	FROM unnested_crates
 ),
 stack_data AS (
+	--  stack_num | stack_contents 
+	-- -----------+----------------
+	-- 			   	0 | ZN
+	-- 				  1 | MCD
+	-- 				  2 | P
 	SELECT
 		stack_num,
 		string_agg(crate, '' ORDER BY row_num DESC) AS stack_contents
@@ -61,15 +67,76 @@ stack_data AS (
 ),
 move_vals AS (
 	SELECT 
+		move,
 		regexp_match(sentence, 'move (\d+) from (\d+) to (\d+)') AS vals
 	FROM day05.instructions
 ),
 moves AS (
 	SELECT 
+		move,
 		vals[1]::int AS amount,
 		vals[2]::int AS origin,
 		vals[3]::int AS destination
 	FROM move_vals
+), 
+initial_stacks AS (
+	--the initial configuration of the stacks
+	-- as the starting point of the recursion
+	SELECT 
+		0 as _round,
+		array_agg(stack_contents ORDER BY stack_num) AS stacks_arr 
+	FROM stack_data
+),
+stack_round AS ( --The actual recursive CTE
+  --initial stacks (non-recursive term)
+	SELECT 
+  	_round, 
+  	stacks_arr 
+  FROM initial_stacks
+  UNION ALL
+	--subsequent stacks (recursive term)
+	SELECT 
+		stack_round._round + 1 as _round,
+		(
+			SELECT array_agg(
+				CASE
+					WHEN stack_num = origin 
+						THEN left(stacks_arr[origin], length(stacks_arr[origin]) - amount)
+					WHEN stack_num = destination
+						THEN stacks_arr[destination] || reverse(right(stacks_arr[origin], amount)) -- append in reverse order!
+				ELSE stack
+				END 
+		  )
+		-- this way is the key to achieving an unnested, enumerated version of the current stacks
+		-- WITHOUT needing to use a subquery referencing the recursive term (see comment below)
+		FROM unnest(stacks_arr) WITH ORDINALITY AS enum(stack, stack_num)
+
+		-- ---This old way achieves the same thing in theory but doesn't work
+		-- ---because a recursive reference to a query cannot appear in a subquery
+		--	      (
+		--	    SELECT 
+		--		    stack, 
+		--		    row_number() over() as stack_num
+		--	    FROM (SELECT unnest(stacks_arr) stack FROM stack_round)q0
+		--	   )q1
+	) 
+	-- and finally, how each recursive term will join on itself to execute each move as a new row
+	FROM stack_round
+	JOIN moves ON (stack_round._round + 1 = moves.move)
+),
+top_crates AS (
+	SELECT 
+		right(unnest(stacks_arr), 1) as crates
+	FROM (
+		SELECT 
+			stacks_arr
+		FROM stack_round
+		ORDER BY _round DESC 
+		LIMIT 1
+	)q
 )
-SELECT * FROM stack_data;
+SELECT 
+	1 AS "Part",
+	string_agg(crates, '') as "Solution"
+FROM top_crates;
 
