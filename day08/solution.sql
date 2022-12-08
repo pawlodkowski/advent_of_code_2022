48fc7a3081e28349dc5193df24630e694c1ff4d1
Paul Wlodkowski <pawlodkowski@gmail.com>
2022-12-08 18:43:24 +0100

    different attempt but misread the problem 🤦

--- day08/solution.sql
+++ day08/solution.sql
@@ -27,34 +27,56 @@ unnested_trees AS (
 --   --   AND
 --   --   col <> (SELECT max(col) FROM unnested_trees) AND col <> 1
 -- ),
-tree_neighbors AS (
+tree_grid AS (
   SELECT
-    row, 
+    row::text || col::text as tree_id,
+    row,
     col,
-    tree,
     CASE 
       WHEN 
         row = (SELECT max(row) FROM unnested_trees) OR row = 1
         OR
         col = (SELECT max(col) FROM unnested_trees) OR col = 1
-      THEN TRUE
-      ELSE FALSE
+      THEN 'true'
+      ELSE 'false'
     END AS "edge",
-    coalesce(lag(tree, -1) OVER(PARTITION BY row ORDER BY col), 0) AS tree_right,
-    coalesce(lag(tree,  1) OVER(PARTITION BY row ORDER BY col), 0) AS tree_left,
-    coalesce(lag(tree, -1) OVER(PARTITION BY col ORDER BY row), 0) AS tree_below,
-    coalesce(lag(tree,  1) OVER(PARTITION BY col ORDER BY row), 0) AS tree_above
-  FROM unnested_trees
+    tree as height
+FROM unnested_trees
+),
+tree_neighbors AS (
+  SELECT
+    *,
+    string_to_array(
+      lag(height::text, -1) OVER(PARTITION BY row ORDER BY col) || ' ' ||
+      lag(edge, -1) OVER(PARTITION BY row ORDER BY col),
+      ' '
+      ) AS tree_right,
+    string_to_array(
+      lag(height::text, 1) OVER(PARTITION BY row ORDER BY col) || ' ' ||
+      lag(edge, 1) OVER(PARTITION BY row ORDER BY col), 
+      ' '
+     ) AS tree_left,
+    string_to_array(
+      lag(height::text, -1) OVER(PARTITION BY col ORDER BY row) || ' ' ||
+      lag(edge, -1) OVER(PARTITION BY col ORDER BY row), 
+      ' '
+     ) AS tree_below,
+    string_to_array(
+      lag(height::text, 1) OVER(PARTITION BY col ORDER BY row) || ' ' ||
+      lag(edge, 1) OVER(PARTITION BY col ORDER BY row), 
+      ' '
+     ) AS tree_above
+  FROM tree_grid
   ORDER BY 1, 2
 )
 SELECT  
   *,
     CASE 
-      WHEN (tree > tree_right) 
-        OR (tree > tree_left) 
-        OR (tree > tree_below) 
-        OR (tree > tree_above)
-        OR edge
+      WHEN (height > tree_right[1]::int AND tree_right[2] = 'true') 
+        OR (height > tree_left[1]::int AND tree_left[2] = 'true') 
+        OR (height > tree_below[1]::int AND tree_below[2] = 'true')  
+        OR (height > tree_above[1]::int AND tree_above[2] = 'true')  
+        OR edge = 'true'
       THEN 1 
       ELSE 0
     END
